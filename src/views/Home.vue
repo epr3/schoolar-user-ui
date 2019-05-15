90feeee12afc72b57db5509458464c42c574a919
Eduard Predescu <eduard.florin.predescu@gmail.com>
2019-05-15 22:59:06 +0300

    WIP image upload

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,19 +4,45 @@
     {{ profile }}
     <p v-if="profile && profile.role === 'PROFESSOR'">Professor</p>
     <p v-if="profile && profile.role === 'STUDENT'">Student</p>
+    <input type="file" @change="previewFiles" />
+    <button @click="uploadFile" class="btn">Test file upload</button>
   </auth-layout>
 </template>
 
 <script>
+import UPLOAD_FILE from '../graphql/Quiz/UploadFile.gql';
+
 import AuthLayout from '../layouts/AuthLayout.vue';
 
 import profileQueryMixin from '../mixins/profileQueryMixin';
 
 export default {
   name: 'home',
+  data: () => ({
+    files: []
+  }),
   mixins: [profileQueryMixin],
   components: {
     AuthLayout
+  },
+  methods: {
+    previewFiles(event) {
+      this.files = event.target.files;
+      console.log(this.files[0]);
+    },
+    async uploadFile() {
+      try {
+        const response = await this.$apollo.mutate({
+          mutation: UPLOAD_FILE,
+          variables: {
+            file: this.files[0]
+          }
+        });
+        console.log(response);
+      } catch (e) {
+        console.log(e);
+      }
+    }
   }
 };
 </script>
